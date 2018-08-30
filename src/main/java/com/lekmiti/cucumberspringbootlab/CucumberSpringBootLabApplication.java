2a2c8e10950d7d04f5209238e62a701a1a8ccfe9
lekmiti <lekmiti1992@gmail.com>
2018-08-30 07:52:05 +0200

    [#2] create a Spring Boot application and add Cucumber dependencies

---
+++ src/main/java/com/lekmiti/cucumberspringbootlab/CucumberSpringBootLabApplication.java
@@ -0,0 +1,12 @@
+package com.lekmiti.cucumberspringbootlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberSpringBootLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberSpringBootLabApplication.class, args);
+	}
+}
