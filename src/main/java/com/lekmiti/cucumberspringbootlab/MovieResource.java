3969aa56fae04ca29146b897b5e7d05f5694f23c
lekmiti <lekmiti1992@gmail.com>
2018-09-11 07:34:16 +0200

    [#6] add getMovie web service that extracts a movie by index

--- src/main/java/com/lekmiti/cucumberspringbootlab/MovieResource.java
+++ src/main/java/com/lekmiti/cucumberspringbootlab/MovieResource.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,17 @@ import java.util.List;
 @RequestMapping("/movie")
 public class MovieResource {
 
+    List<String> movies = Arrays.asList("Catch me if you can", "Transformers ", "Transporter");
+
     @GetMapping("/all")
     public ResponseEntity<List<String>> getAllMovies() {
-        List<String> movies = Arrays.asList("Catch me if you can", "Transformers ", "Transporter");
         return new ResponseEntity<>(movies, new HttpHeaders(), HttpStatus.OK);
     }
+
+    @GetMapping("/{id}" )
+    public ResponseEntity<String> getMovie(@PathVariable("id") int id) {
+        return id < movies.size() ?
+                new ResponseEntity<>(movies.get(id), new HttpHeaders(), HttpStatus.OK) :
+                new ResponseEntity<>(null, new HttpHeaders(), HttpStatus.NO_CONTENT);
+      }
 }
